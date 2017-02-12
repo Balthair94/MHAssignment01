f169046072b01f2a0defe87f8fe33a4b4b4640f7
balthair94 <archerbaltazar@gmail.com>
2017-02-12 15:38:55 -0600

    Projects section added

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function showHide(id) {
+	var cName = document.getElementById(id).getAttribute("class");
+	console.log(cName)
+	if (cName == "table-tr-hide") {
+		document.getElementById(id).className = "table-tr-show"
+	} else {
+		document.getElementById(id).className = "table-tr-hide"
+	}
+}

2863209cd2215a412502cb2f02d3d4ca47d2c629
balthair94 <archerbaltazar@gmail.com>
2017-02-12 18:23:42 -0600

    footer added

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 function showHide(id) {
 	var cName = document.getElementById(id).getAttribute("class");
-	console.log(cName)
 	if (cName == "table-tr-hide") {
 		document.getElementById(id).className = "table-tr-show"
 	} else {
@@ -10,5 +9,5 @@ function showHide(id) {
 
 function moveTo(id){
 	element = document.getElementById(id)
-	element.scrollIntoView(true);
+	element.scrollIntoView();
 }

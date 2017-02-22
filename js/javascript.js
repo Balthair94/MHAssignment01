bb231994de565ce1c5cbd5b51c84d8068b3dbee7
balthair94 <archerbaltazar@gmail.com>
2017-02-22 00:50:41 -0600

    Issue 7 fixed

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,35 @@
 function showHide(id) {
+
+	var proj = ['app_description_01', 'app_description_02', 'app_description_03', 'app_description_04', 'app_description_05'];
+
+	//Getting the current class of the element
 	var cName = document.getElementById(id).getAttribute("class");
+
+	//Element is visible
 	if (cName == "table-tr-hide") {
+		for (var i = 0; i < proj.length; i++) {
+			document.getElementById(proj[i]).className = "table-tr-hide"
+		}
+		//Hide element
 		document.getElementById(id).className = "table-tr-show"
-	} else {
+	} 
+	//Element is hide
+	else {
+		//Show element
 		document.getElementById(id).className = "table-tr-hide"
 	}
 }
 
 function moveTo(id){
+	//Getting the element
 	element = document.getElementById(id)
+	//Scrilling to the element
 	element.scrollIntoView(true);
 }
 
-$('.avatar-container').on('click',function(){
+
+//CODE FROM THE LIBRARY USEDED
+$('.avatar-container').hover(function(){
   
   if ($(this).find('.info').length <= 0) {
     $(this).append('<div class="info"><div class="info-inner"></div></div>');
